44330f72a44cf93a1cb0bb95fe969a64d7f21d4e
Md Nahidul Islam <nahidulislamuiu@gmail.com>
2022-03-25 00:04:51 +0600

    school_management

---
+++ school_management/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\UserController;
+use App\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('admin.index');
+})->name('dashboard');
+Route::get('/admin/logout', [AdminController::class, 'Logout'])->name('admin.logout');
+
+
+//User Management All Routes
+
+Route::prefix('users')->group(function () {
+
+    Route::get('/view', [UserController::class, 'UserView'])->name('user.view');
+    Route::get('/add', [UserController::class, 'UserAdd'])->name('user.add');
+    Route::post('/store', [UserController::class, 'UserStore'])->name('users.store');
+    Route::get('/edit/{id}', [UserController::class, 'UserEdit'])->name('users.edit');
+    Route::post('/update/{id}', [UserController::class, 'UserUpdate'])->name('users.update');
+    Route::get('/delete/{id}', [UserController::class, 'UserDelete'])->name('users.delete');
+});
