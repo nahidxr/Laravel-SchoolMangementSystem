6acd4a578d530dc9bf02474aa48a0d4f4ab0b3d4
Md Nahidul Islam <nahidulislamuiu@gmail.com>
2022-03-26 14:39:29 +0600

    edited password

--- school_management/app/Http/Controllers/Backend/ProfileController.php
+++ school_management/app/Http/Controllers/Backend/ProfileController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -50,5 +51,33 @@ class ProfileController extends Controller
 
         );
         return redirect()->route('profile.view')->with($notification);
+    }
+
+    public function PasswordView()
+    {
+
+
+        return view('backend.user.edit_password');
+    }
+
+    public function PasswordUpdate(Request $request)
+    {
+
+        $validatedData = $request->validate([
+
+            'oldpassword' => 'required',
+            'password' => 'required|confirmed',
+
+        ]);
+        $hashedPassword = Auth::user()->password;
+        if (Hash::check($request->oldpassword, $hashedPassword)) {
+            $user = User::find(Auth::id());
+            $user->password = Hash::make($request->password);
+            $user->save();
+            Auth::logout();
+            return redirect()->route('login');
+        } else {
+            return redirect()->back();
+        }
     }
 }
